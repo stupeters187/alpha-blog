5959ff573fbb8de40add23afe5f4e725dd029680
Stu Peters <stu_peters187@hotmail.com>
2016-07-27 18:33:43 -0400

    Add article and category assoc

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ActiveRecord::Base
 
   belongs_to :user
 
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+
   validates :title, presence: true, 
             length: {minimum: 3, maximum: 50}
   
